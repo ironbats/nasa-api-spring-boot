7bef1740d634edca42a49419d036a1a6b6966f5f
Felipe Rodrigues <felipe.rodrigues@keyrus.com.br>
2018-11-19 16:35:13 -0200

    implements docker pattern

--- src/main/java/br/com/adtech/AddtechNeasApplication.java
+++ src/main/java/br/com/adtech/AddtechNeasApplication.java
@@ -3,8 +3,6 @@ package br.com.adtech;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import br.com.adtech.api.AdtechConsumer;
-
 @SpringBootApplication
 public class AddtechNeasApplication {
 
