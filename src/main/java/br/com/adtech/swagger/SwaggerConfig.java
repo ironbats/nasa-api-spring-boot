0fe1f38b93237c9ef0659e0b1cb00209df9ed972
Felipe Rodrigues <felipe.rodrigues@keyrus.com.br>
2019-07-29 14:43:53 -0300

    improve swagger

--- src/main/java/br/com/adtech/swagger/SwaggerConfig.java
+++ src/main/java/br/com/adtech/swagger/SwaggerConfig.java
@@ -3,8 +3,11 @@ package br.com.adtech.swagger;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -12,13 +15,35 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @Configuration
 @EnableSwagger2
 @EnableAutoConfiguration
-public class SwaggerConfig {
+public class SwaggerConfig  extends WebMvcConfigurationSupport {
 
 	@Bean
 	public Docket api() {
 		return new Docket(DocumentationType.SWAGGER_2).select()
-				.apis(RequestHandlerSelectors.any()).paths(PathSelectors.any())
-				.build();
+				.apis(RequestHandlerSelectors.basePackage("br.com.adtech"))
+				.paths(PathSelectors.any())
+				.build().apiInfo(apiInfo());
+	}
+
+
+	private ApiInfo apiInfo() {
+		return new ApiInfo(
+				"The bots Api Messages",
+				"Application to Send Messages between anonymous Users and Bots applications",
+				"v1.0",
+				null,
+				null,
+				null, null);
+	}
+
+
+	@Override
+	protected void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("swagger-ui.html")
+				.addResourceLocations("classpath:/META-INF/resources/");
+
+		registry.addResourceHandler("/webjars/**")
+				.addResourceLocations("classpath:/META-INF/resources/webjars/");
 	}
 
 }
