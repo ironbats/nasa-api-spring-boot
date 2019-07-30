899ecee85dec45007b5e6f7c112ef35288c0b910
Felipe Rodrigues <felipe.rodrigues@keyrus.com.br>
2019-07-30 00:42:56 -0300

    improve pom.xml

--- src/main/java/br/com/adtech/swagger/SwaggerConfig.java
+++ src/main/java/br/com/adtech/swagger/SwaggerConfig.java
@@ -3,7 +3,6 @@ package br.com.adtech.swagger;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 import springfox.documentation.builders.PathSelectors;
@@ -15,7 +14,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
 @EnableSwagger2
-@EnableWebMvc
+@EnableAutoConfiguration
 public class SwaggerConfig  extends WebMvcConfigurationSupport {
 
 	@Bean
