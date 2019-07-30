2b4fd60cac1ceb2b33f2e2c701bab15dc686270b
Felipe Rodrigues <felipe.rodrigues@keyrus.com.br>
2019-07-30 01:29:05 -0300

    improve swagger and application

--- src/main/java/br/com/adtech/AddtechNeasApplication.java
+++ src/main/java/br/com/adtech/AddtechNeasApplication.java
@@ -4,12 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
-@ComponentScan(basePackages = "br.com.adtech.*")
-@EnableAsync
+@ComponentScan(basePackages = {"br.com.adtech.service",
+		"br.com.adtech.controller","br.com.adtech.swagger","br.com.adtech.security"})
 public class AddtechNeasApplication {
 
 	public static void main(String[] args) {
--- src/main/java/br/com/adtech/swagger/SwaggerConfig.java
+++ src/main/java/br/com/adtech/swagger/SwaggerConfig.java
@@ -3,14 +3,21 @@ package br.com.adtech.swagger;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.service.VendorExtension;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
 
 @Configuration
 @EnableSwagger2
@@ -27,13 +34,24 @@ public class SwaggerConfig  extends WebMvcConfigurationSupport {
 
 
 	private ApiInfo apiInfo() {
+		Collection<VendorExtension> vendorExtensions = new ArrayList<>();
 		return new ApiInfo(
-				"The bots Api Messages",
-				"Application to Send Messages between anonymous Users and Bots applications",
-				"v1.0",
-				null,
-				null,
-				null, null);
+						"The Nasa Api Messages",
+						"Application get information to asteroids ",
+						"v1.0",
+						"swagger",new Contact("Felipe Rodrigues","swagger","felipe.themarkiron"), "felipe.themarkiron@gmail.com",
+				"",
+				Arrays.asList(new VendorExtension() {
+					@Override
+					public String getName() {
+						return "Felipe Rodrigues";
+					}
+
+					@Override
+					public Object getValue() {
+						return null;
+					}
+				}));
 	}
 
 
