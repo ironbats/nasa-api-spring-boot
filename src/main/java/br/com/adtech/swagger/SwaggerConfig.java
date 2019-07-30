696e991093f67b89e1defbfc12424a810fa44f2f
Felipe Rodrigues <felipe.rodrigues@keyrus.com.br>
2019-07-30 01:48:54 -0300

    improve pom.xml

--- src/main/java/br/com/adtech/swagger/SwaggerConfig.java
+++ src/main/java/br/com/adtech/swagger/SwaggerConfig.java
@@ -3,21 +3,17 @@ package br.com.adtech.swagger;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
-import springfox.documentation.service.VendorExtension;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collection;
+
 
 @Configuration
 @EnableSwagger2
@@ -34,24 +30,13 @@ public class SwaggerConfig  extends WebMvcConfigurationSupport {
 
 
 	private ApiInfo apiInfo() {
-		Collection<VendorExtension> vendorExtensions = new ArrayList<>();
+
 		return new ApiInfo(
 						"The Nasa Api Messages",
 						"Application get information to asteroids ",
 						"v1.0",
 						"swagger",new Contact("Felipe Rodrigues","swagger","felipe.themarkiron"), "felipe.themarkiron@gmail.com",
-				"",
-				Arrays.asList(new VendorExtension() {
-					@Override
-					public String getName() {
-						return "Felipe Rodrigues";
-					}
-
-					@Override
-					public Object getValue() {
-						return null;
-					}
-				}));
+				"Felipe");
 	}
 
 
